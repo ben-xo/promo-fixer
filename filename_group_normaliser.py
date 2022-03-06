be4b69bc8edca0a8736e77b226225d5ea3924e74
Ben Isaacs <75862+ben-xo@users.noreply.github.com>
2022-03-06 21:07:46 +0000

    Make it work for more than 2 files on a release, and check it doesn't break the normal case

--- filename_group_normaliser.py
+++ filename_group_normaliser.py
@@ -8,11 +8,14 @@ class FilenameGroupNormaliser:
     def __init__(self, fnps: List[FilenameParser]):
         self.fnps = fnps
 
-    def normalise(self) -> None:
+    def normalise(self) -> int:
+        mutation_count = 0
         if len(self.fnps) > 1:
-            self.fix_three_part_artist_title_label()
+            mutation_count += self.fix_three_part_artist_title_label()
+        return mutation_count
 
-    def fix_three_part_artist_title_label(self) -> None:
+    def fix_three_part_artist_title_label(self) -> int:
+        mutation_count = 0
         three_part_fnps = [fnp for fnp in self.fnps if fnp.label != '']
 
         # start a list of fnps with titles that are identical.
@@ -21,10 +24,15 @@ class FilenameGroupNormaliser:
             for fnp in three_part_fnps:
                 if fnp.title == identical_titles[0].title and fnp.artist != identical_titles[0].artist:
                     identical_titles.append(fnp)
-                    three_part_fnps.remove(fnp)
 
             if len(identical_titles) > 1:
                 # at least 2 files have the same title but different artists.
                 # this implies that instead of label - artist - title it's actually artist - title - label
                 for fnp in identical_titles:
                     (fnp.label, fnp.artist, fnp.title) = (fnp.title, fnp.label, fnp.artist)
+                    mutation_count += 1
+
+                for fnp in identical_titles[1:]:
+                    three_part_fnps.remove(fnp)
+
+        return mutation_count
--- test_filename_group_normaliser.py
+++ test_filename_group_normaliser.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from filename_parser import FilenameParser
 from filename_group_normaliser import FilenameGroupNormaliser
+from test_filename_parser import CASES as NEGATIVE_CASES
 
 CASES = [
     ([
@@ -9,7 +10,15 @@ CASES = [
     ], [
         ('HEART TWICE 008', 'UPHONIX', 'Shimmer'),
         ('HEART TWICE 008', 'UPHONIX', 'Purple Dub'),
-    ])
+    ]), ([
+        'SEXTASY - check point - HEART TWICE 006.wav',
+        'SEXTASY - far beyond - HEART TWICE 006.wav',
+        'SEXTASY - give it back - HEART TWICE 006.wav',
+    ], [
+        ('HEART TWICE 006', 'SEXTASY', 'check point'),
+        ('HEART TWICE 006', 'SEXTASY', 'far beyond'),
+        ('HEART TWICE 006', 'SEXTASY', 'give it back'),
+    ]),
 ]
 
 
@@ -27,11 +36,25 @@ class FilenameGroupNormaliserTestCase(TestCase):
             assert len(fnps) == len(infiles)
 
             fnp_group = FilenameGroupNormaliser(fnps)
-            fnp_group.normalise()
+            mutation_count = fnp_group.normalise()
+            assert mutation_count > 0
             with self.subTest(msg=', '.join(infiles)):
                 for (fnp, lat) in zip(fnps, label_artist_titles):
                     assert fnp.label == lat[0], "label: expected {}, got {}".format(lat[0], fnp.label)
                     assert fnp.artist == lat[1], "artist: expected {}, got {}".format(lat[1], fnp.artist)
                     assert fnp.title == lat[2], "title: expected {}, got {}".format(lat[2], fnp.title)
 
+    def test_filename_group_normalising_doesnt_break_ordinary_things(self):
+        # this is a little contorted because the NEGATIVE_CASES are pairs of (filename, (label, artist, title))
+
+        # convert it to a list of (FilenameParser(filename), (label, artist, title)) and analyse them
+        fnps = [(FilenameParser(infile), lat) for infile, lat in NEGATIVE_CASES]
+        assert len(fnps) == len(NEGATIVE_CASES)
+        for fnp in fnps:
+            fnp[0].analyse()
+
+        # now we need just a list of the FilenameParsers.
+        fnp_group = FilenameGroupNormaliser([fnp[0] for fnp in fnps])
+        mutation_count = fnp_group.normalise()
+        assert mutation_count == 0
 
